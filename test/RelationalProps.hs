ec7a5ee2a53d3dc1ccfd7e26c68aef35160853d1
Kevin Charter <kevin.charter@acm.org>
2010-08-19 21:48:40 -0600

    Split remaining relational 'props' into 'prop', 'propM' versions.
    I've split the prop functions for the 'rename' properties into
    'propM' functions that return their results in an error monad,
    and 'prop' functions that just compose 'noErr' with 'propM'.

--- test/RelationalProps.hs
+++ test/RelationalProps.hs
@@ -23,32 +23,42 @@ propM_makeSigAndTuples mk (names, tuples) =
 
 
 prop_canRenameExistingToItself :: (R.Relational n d r, Eq r) => r -> Bool
-prop_canRenameExistingToItself r =
-    noErr $ do s <- R.signature r
-               (if null s
-                then return True
-                else let n = head s
-                     in (r==) `liftM` R.rename n n r)
+prop_canRenameExistingToItself = noErr . propM_canRenameExistingToItself
+
+propM_canRenameExistingToItself :: (R.Relational n d r, Eq r, Error e, MonadError e m) => r -> m Bool
+propM_canRenameExistingToItself r =
+    do s <- R.signature r
+       (if null s
+        then return True
+        else let n = head s
+             in (r==) `liftM` R.rename n n r)
 
 prop_renameIsReversible :: (R.Relational n d r, Eq r) => (r, n) -> Bool
-prop_renameIsReversible (r, n) =
-    noErr $ do s <- R.signature r
-               (if null s || n `elem` s
-                then return True
-                else let n' = head s
-                     in do r' <- R.rename n' n r
-                           r'' <- R.rename n n' r'
-                           return (r' == r''))
+prop_renameIsReversible = noErr . propM_renameIsReversible
+
+propM_renameIsReversible :: (R.Relational n d r, Eq r, Error e, MonadError e m) => (r, n) -> m Bool
+propM_renameIsReversible (r, n) =
+    do s <- R.signature r
+       (if null s || n `elem` s
+        then return True
+        else let n' = head s
+             in do r' <- R.rename n' n r
+                   r'' <- R.rename n n' r'
+                   return (r' == r''))
 
 prop_canRemoveIntermediateRenames :: (R.Relational n d r, Eq r) => (r, n, n) -> Bool
-prop_canRemoveIntermediateRenames (r, n, m) =
-    noErr $ do s <- R.signature r
-               (if null s || n `elem` s || m `elem` s
-                then return True
-                else do r' <- R.rename (head s) n r
-                        r'' <- R.rename n m r'
-                        r''' <- R.rename (head s) m r
-                        return (r' == r'''))
+prop_canRemoveIntermediateRenames = noErr . propM_canRemoveIntermediateRenames
+
+propM_canRemoveIntermediateRenames :: (R.Relational n d r, Eq r, Error e, MonadError e m) =>
+                                      (r, n, n) -> m Bool
+propM_canRemoveIntermediateRenames (r, n, m) =
+    do s <- R.signature r
+       (if null s || n `elem` s || m `elem` s
+        then return True
+        else do r' <- R.rename (head s) n r
+                r'' <- R.rename n m r'
+                r''' <- R.rename (head s) m r
+                return (r' == r'''))
                
 
 noErr :: Either String b -> Bool
