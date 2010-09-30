59212893c50d8fa0c07f61dabc2ed96269537141
Kevin Charter <kevin.charter@acm.org>
2010-09-29 22:48:52 -0600

    Added a generator for satisfiable Conditions.
    For testing 'select' and 'join', we'll need to be able to
    generate satisfiable conditions. On value types which are
    Bounded, Enum, and Ord, I've added a brute force generator
    that generates random conditions until it finds one
    that can be satisfied. For small signatures and small
    conditions I think this will perform OK, but I'll see in
    the next few commits.

--- test/ConditionGen.hs
+++ test/ConditionGen.hs
@@ -2,10 +2,15 @@
 
 module ConditionGen where
 
-import Control.Monad (liftM, liftM2, liftM3)
+import Control.Monad (liftM, liftM2, liftM3, foldM, join)
+import Control.Monad.Error (throwError)
+import Data.List (nub, sort, foldl')
+import qualified Data.Map as DM
+import Data.Maybe (catMaybes, mapMaybe)
 import Test.QuickCheck
 
 import Relational.Condition
+import MonadUtil (untilM)
 
 expression :: (Arbitrary d, CoArbitrary [d], Arbitrary (m d)) => Gen n -> Int -> Gen (Expression n d m)
 expression name size =
@@ -33,3 +38,53 @@ condition name size =
                  liftM2 CondCall arbitrary (listOf subExpression)]
     where subCondition = condition name (size `div` 2)
           subExpression = expression name (size `div`2)
+
+satisfiableCondition :: (Ord n, Show n, Ord d, Bounded d, Enum d, Arbitrary d, CoArbitrary d) =>
+                        [n] -> Int -> Gen (Condition n d (Either String))
+satisfiableCondition names size =
+    untilM isSatisfiable (condition (elements names) size)
+
+isSatisfiable :: (Ord n, Ord d, Bounded d, Enum d) => Condition n d (Either String) -> Bool
+isSatisfiable c = maybe False (const True) (thatSatisfy names c)
+    where names = attrNamesIn c
+
+attrNamesIn c =
+    case c of CondNot c' -> attrNamesIn c'
+              CondAnd c' c'' -> attrNamesIn c' ++ attrNamesIn c''
+              CondOr c' c'' -> attrNamesIn c' ++ attrNamesIn c''
+              CondRel _ e e' -> attrNamesInExp e ++ attrNamesInExp e'
+              CondCall _ exps -> concatMap attrNamesInExp exps
+              _ -> []
+    where attrNamesInExp e =
+              case e of ExpValueOf n -> [n]
+                        ExpCall _ exps -> concatMap attrNamesInExp exps
+                        _ -> []
+
+thatSatisfy :: (Ord n, Ord d, Bounded d, Enum d) =>
+               [n] -> Condition n d (Either String) -> Maybe (Gen [(n,d)])
+thatSatisfy names c = if null satisfying' then Nothing else Just (elements satisfying')
+    where satisfying' = map DM.toList $ satisfying c $ allTuples names
+
+satisfying :: (Ord n, Ord d) =>
+              Condition n d (Either String) -> [DM.Map n d] -> [DM.Map n d]
+satisfying c = filter (\m -> evalOn m)
+    where evalOn m = either (const False) id (evalCondition (lookup m) c)
+          lookup m n = maybe (noSuchName n) return (DM.lookup n m)
+          noSuchName n = throwError "Bad name (ignored)"
+
+allTuples :: (Bounded d, Enum d, Ord n) => [n] -> [DM.Map n d]
+allTuples [] = [DM.empty]
+allTuples names =
+    let names' = reverse $ nub $ sort names
+        sets = replicate (length names') [minBound .. maxBound]
+        singletons = map (:[]) (head sets)
+        tuples = foldl' (liftM2 (flip (:))) singletons (tail sets)
+    in map (DM.fromList . reverse . zip names') tuples
+
+data Small = One | Two | Three | Four | Five deriving (Eq, Ord, Show, Bounded, Enum)
+
+instance Arbitrary Small where
+    arbitrary = elements [minBound..maxBound]
+
+instance CoArbitrary Small where
+    coarbitrary s = variant (fromEnum s)
