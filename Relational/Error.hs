69593320218150880defc059290e0c532cc55295
Kevin Charter <kevin.charter@acm.org>
2010-12-02 21:38:35 -0700

    Add the Relational.Error module.
    Currently, the Relational class permits an unnecessarily wide
    range of monads. For example, any error monad is permitted.
    This isn't what we want, since different kinds of relations
    may need to be closely associated with particular kinds of
    monads. Further, there are situations where we may want
    to be able to do IO in certain implementations -- ones backed
    by a real database, for example, or file system storage of
    some kind.
    
    If we're going to restrict the monads, we might as well
    have a standard scheme for errors too. Here is the current
    error type. In the next couple of commits, I'll require
    that the monads be instances of (MonadError (RelationalError n)).

---
+++ Relational/Error.hs
@@ -0,0 +1,29 @@
+-- | Special-purpose errors for relational operations. The
+-- 'RelationalError' type is an instance of 'Error' and captures
+-- common errors in relational operations.
+
+module Relational.Error where
+
+import Control.Monad.Error (Error(..))
+
+import Relational.RelVar
+
+-- | Special-purpose errors for relational operations.
+data RelationalError n =
+    DuplicatedName n |
+    -- ^ A name is duplicated in 'make'.
+    NoSuchName n |
+    -- ^ An expression references an attribute that doesn't exist.
+    DifferingSignatures [n] [n] |
+    -- ^ Two signatures differ, and so are not union-compatible.
+    OverlappingSignatures [n] [n] |
+    -- ^ Two signatures are not disjoint, and so are not product-compatible.
+    NoSuchRelVar RelVar |
+    -- ^ Reference to a non-existent relational variable name.
+    GeneralError String
+    -- ^ A catch-all for all other errors.
+    deriving (Eq, Ord, Show)
+
+instance Error (RelationalError n) where
+    strMsg = GeneralError
+
