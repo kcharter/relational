18391f8e36c2de18169841803e6f4a8d8055d154
Kevin Charter <kevin.charter@acm.org>
2010-12-30 11:52:02 -0700

    Remove Relational.Naive.Error.
    This tiny module isn't used anymore; remove it. The package
    still builds, and the unit tests still run successfully.

--- Relational/Naive/Error.hs
+++
@@ -1,7 +0,0 @@
-module Relational.Naive.Error (die) where
-
-import Control.Monad.Error (Error, MonadError, throwError, strMsg)
-
-die :: (Error e, MonadError e m) => String -> m a
-die = throwError . strMsg
-
